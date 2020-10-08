91655d0e426a0ae94c2354387fc2e9b6a8877e91
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2020-10-08 20:05:31 +0630

    Fresh Rails 5 App with Vue and Postgresql

---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module PmApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.javascript_engine :js
+      g.helper            false
+      g.system_tests      false
+      g.test_framework    nil
+      g.jbuilder          false
+    end
+  end
+end
